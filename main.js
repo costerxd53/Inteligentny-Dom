e0337417bf383332caaec87ebfaca26f4b7317bc
costerxd53 <59176646+costerxd53@users.noreply.github.com>
2020-02-12 21:16:22 +0100

    Add files via upload

--- main.js
+++ main.js
@@ -1,5 +1,23 @@
-var ip_home = "http://10.0.1.198:8694";
-var ip_outdoor = "http://77.252.137.224:8694";
+  document.getElementById('wyp').style = "display: none";
+    var ip = "http://10.0.1.198:8694";
+var wifi = document.getElementById("wifi");
+var lte = document.getElementById("lte");
+
+wifi.addEventListener("click", continue_wifi, false);
+lte.addEventListener("click", continue_lte, false);
+
+
+function continue_wifi() {
+ ip = "http://10.0.1.198:8694";
+  document.getElementById('wyp').style = "display: visible";
+  document.getElementById('pre').style = "display: none";
+}
+function continue_lte() {
+ ip = "http://77.252.137.224:8694";
+  document.getElementById('wyp').style = "display: visible";
+    document.getElementById('pre').style = "display: none";
+}
+
 
 var switch_1 = document.getElementById("switch_2");
 var switch_2 = document.getElementById("switch_1");
@@ -20,34 +38,12 @@ switch_check_2_load();
 switch_check_3_load();
 
 function switch_check_1() {
-$.get(ip_home+"/switch_1_check", function(data, status){
+$.get(ip+"/switch_1_check", function(data, status){
   //alert("Data: " + data + "\nStatus: " + status);
   if(status != "success") {
-    $.get(ip_outdoor+"/switch_1_check", function(data, status){
-      //alert("Data: " + data + "\nStatus: " + status);
-      if(status != "success") {
-        //definitly not work
-        error();
-      }
-      else {
-        //work ip_outdoor
-        if(data == "0") {
-          //on
-          switch_off_1()
-        }
-        else if(data == "1") {
-          //off
-          switch_on_1();
-        }
-        else {
-          //error
-          error();
-        }
-      }
-      });
   }
   else {
-    //work ip_home
+    //work ip
     if(data == "0") {
       //on
       switch_off_1()
@@ -56,43 +52,18 @@ $.get(ip_home+"/switch_1_check", function(data, status){
       //off
       switch_on_1();
     }
-    else {
-      //error
-      error();
-    }
+
   }
 });
 }
 
 function switch_check_2() {
-$.get(ip_home+"/switch_2_check", function(data, status){
+$.get(ip+"/switch_2_check", function(data, status){
   //alert("Data: " + data + "\nStatus: " + status);
   if(status != "success") {
-    $.get(ip_outdoor+"/switch_2_check", function(data, status){
-      //alert("Data: " + data + "\nStatus: " + status);
-      if(status != "success") {
-        //definitly not work
-        error();
-      }
-      else {
-        //work ip_outdoor
-        if(data == "0") {
-          //on
-          switch_off_2()
-        }
-        else if(data == "1") {
-          //off
-          switch_on_2();
-        }
-        else {
-          //error
-          error();
-        }
-      }
-      });
   }
   else {
-    //work ip_home
+    //work ip
     if(data == "0") {
       //on
       switch_off_2()
@@ -110,34 +81,12 @@ $.get(ip_home+"/switch_2_check", function(data, status){
 }
 
 function switch_check_3() {
-$.get(ip_home+"/switch_4_check", function(data, status){
+$.get(ip+"/switch_4_check", function(data, status){
   //alert("Data: " + data + "\nStatus: " + status);
   if(status != "success") {
-    $.get(ip_outdoor+"/switch_4_check", function(data, status){
-      //alert("Data: " + data + "\nStatus: " + status);
-      if(status != "success") {
-        //definitly not work
-        error();
-      }
-      else {
-        //work ip_outdoor
-        if(data == "0") {
-          //on
-          switch_off_3()
-        }
-        else if(data == "1") {
-          //off
-          switch_on_3();
-        }
-        else {
-          //error
-          error();
-        }
-      }
-      });
   }
   else {
-    //work ip_home
+    //work ip
     if(data == "0") {
       //on
       switch_off_3()
@@ -155,34 +104,12 @@ $.get(ip_home+"/switch_4_check", function(data, status){
 }
 
 function switch_check_1_load() {
-$.get(ip_home+"/switch_1_check", function(data, status){
+$.get(ip+"/switch_1_check", function(data, status){
   //alert("Data: " + data + "\nStatus: " + status);
   if(status != "success") {
-    $.get(ip_outdoor+"/switch_1_check", function(data, status){
-      //alert("Data: " + data + "\nStatus: " + status);
-      if(status != "success") {
-        //definitly not work
-        error();
-      }
-      else {
-        //work ip_outdoor
-        if(data == "0") {
-          //on
-        switch_1.style.background = "green";
-        }
-        else if(data == "1") {
-          //off
-        switch_1.style.background = "white";
-        }
-        else {
-          //error
-          error();
-        }
-      }
-      });
   }
   else {
-    //work ip_home
+    //work ip
     if(data == "0") {
       //on
     switch_1.style.background = "green";
@@ -200,34 +127,12 @@ $.get(ip_home+"/switch_1_check", function(data, status){
 }
 
 function switch_check_2_load() {
-$.get(ip_home+"/switch_2_check", function(data, status){
+$.get(ip+"/switch_2_check", function(data, status){
   //alert("Data: " + data + "\nStatus: " + status);
   if(status != "success") {
-    $.get(ip_outdoor+"/switch_2_check", function(data, status){
-      //alert("Data: " + data + "\nStatus: " + status);
-      if(status != "success") {
-        //definitly not work
-        error();
-      }
-      else {
-        //work ip_outdoor
-        if(data == "0") {
-          //on
-        switch_2.style.background = "green";
-        }
-        else if(data == "1") {
-          //off
-        switch_2.style.background = "white";
-        }
-        else {
-          //error
-          error();
-        }
-      }
-      });
   }
   else {
-    //work ip_home
+    //work ip
     if(data == "0") {
       //on
       switch_2.style.background = "green";
@@ -245,34 +150,12 @@ $.get(ip_home+"/switch_2_check", function(data, status){
 }
 
 function switch_check_3_load() {
-$.get(ip_home+"/switch_4_check", function(data, status){
+$.get(ip+"/switch_4_check", function(data, status){
   //alert("Data: " + data + "\nStatus: " + status);
   if(status != "success") {
-    $.get(ip_outdoor+"/switch_4_check", function(data, status){
-      //alert("Data: " + data + "\nStatus: " + status);
-      if(status != "success") {
-        //definitly not work
-        error();
-      }
-      else {
-        //work ip_outdoor
-        if(data == "0") {
-          //on
-          switch_3.style.background = "green";
-        }
-        else if(data == "1") {
-          //off
-          switch_3.style.background = "white";
-        }
-        else {
-          //error
-          error();
-        }
-      }
-      });
   }
   else {
-    //work ip_home
+    //work ip
     if(data == "0") {
       //on
           switch_3.style.background = "green";
@@ -294,138 +177,72 @@ $.get(ip_home+"/switch_4_check", function(data, status){
 
 
   function switch_on_1() {
-    $.get(ip_home+"/switch_1_on", function(data, status){
+    $.get(ip+"/switch_1_on", function(data, status){
       //alert("Data: " + data + "\nStatus: " + status);
       if(status != "success") {
-        $.get(ip_outdoor+"/switch_1_on", function(data, status){
-          //alert("Data: " + data + "\nStatus: " + status);
-          if(status != "success") {
-            //definitly not work
-            switch_1.style.background = "orange";
-          }
-          else {
-            //work ip_outdoor
-            switch_1.style.background = "green";
-          }
-          });
       }
       else {
-        //work ip_home
+        //work ip
         switch_1.style.background = "green";
       }
     });
 }
 
 function switch_on_2() {
-  $.get(ip_home+"/switch_2_on", function(data, status){
+  $.get(ip+"/switch_2_on", function(data, status){
     //alert("Data: " + data + "\nStatus: " + status);
     if(status != "success") {
-      $.get(ip_outdoor+"/switch_2_on", function(data, status){
-        //alert("Data: " + data + "\nStatus: " + status);
-        if(status != "success") {
-          //definitly not work
-          switch_2.style.background = "orange";
-        }
-        else {
-          //work ip_outdoor
-          switch_2.style.background = "green";
-        }
-        });
     }
     else {
-      //work ip_home
+      //work ip
       switch_2.style.background = "green";
     }
   });
 }
 
 function switch_on_3() {
-  $.get(ip_home+"/switch_4_on", function(data, status){
+  $.get(ip+"/switch_4_on", function(data, status){
     //alert("Data: " + data + "\nStatus: " + status);
     if(status != "success") {
-      $.get(ip_outdoor+"/switch_4_on", function(data, status){
-        //alert("Data: " + data + "\nStatus: " + status);
-        if(status != "success") {
-          //definitly not work
-          switch_3.style.background = "orange";
-        }
-        else {
-          //work ip_outdoor
-          switch_3.style.background = "green";
-        }
-        });
     }
     else {
-      //work ip_home
+      //work ip
       switch_3.style.background = "green";
     }
   });
 }
 
 function switch_off_1() {
-  $.get(ip_home+"/switch_1_off", function(data, status){
+  $.get(ip+"/switch_1_off", function(data, status){
     //alert("Data: " + data + "\nStatus: " + status);
     if(status != "success") {
-      $.get(ip_outdoor+"/switch_1_off", function(data, status){
-        //alert("Data: " + data + "\nStatus: " + status);
-        if(status != "success") {
-          //definitly not work
-          switch_1.style.background = "orange";
-        }
-        else {
-          //work ip_outdoor
-          switch_1.style.background = "white";
-        }
-        });
     }
     else {
-      //work ip_home
+      //work ip
       switch_1.style.background = "white";
     }
   });
 }
 
 function switch_off_2() {
-  $.get(ip_home+"/switch_2_off", function(data, status){
+  $.get(ip+"/switch_2_off", function(data, status){
     //alert("Data: " + data + "\nStatus: " + status);
     if(status != "success") {
-      $.get(ip_outdoor+"/switch_2_off", function(data, status){
-        //alert("Data: " + data + "\nStatus: " + status);
-        if(status != "success") {
-          //definitly not work
-          switch_2.style.background = "orange";
-        }
-        else {
-          //work ip_outdoor
-          switch_2.style.background = "white";
-        }
-        });
     }
     else {
-      //work ip_home
+      //work ip
       switch_2.style.background = "white";
     }
   });
 }
 
 function switch_off_3() {
-  $.get(ip_home+"/switch_4_off", function(data, status){
+  $.get(ip+"/switch_4_off", function(data, status){
     //alert("Data: " + data + "\nStatus: " + status);
     if(status != "success") {
-      $.get(ip_outdoor+"/switch_4_off", function(data, status){
-        //alert("Data: " + data + "\nStatus: " + status);
-        if(status != "success") {
-          //definitly not work
-          switch_3.style.background = "orange";
-        }
-        else {
-          //work ip_outdoor
-          switch_3.style.background = "white";
-        }
-        });
     }
     else {
-      //work ip_home
+      //work ip
       switch_3.style.background = "white";
     }
   });
